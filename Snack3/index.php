93b491d48e8794e2c22236afb0811287ff8579fa
StefanoMRC <stefano.mrc92@gmail.com>
2022-05-05 14:58:43 +0200

    fine snack3

--- Snack3/index.php
+++ Snack3/index.php
@@ -1,1 +1,15 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+    $array=[];
+    while (count($array) < 15) {
+        $numero=rand(1,100);
+        if (!in_array($numero, $array)) {
+            $array[]=$numero;
+        }
+    }
+    var_dump($array);
+    for ($i=0; $i < count($array); $i++) { 
+       echo "<p>". "$array[$i]" ."</p>";
+    }
+?>
